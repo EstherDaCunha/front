e0d5a3452bf00eb6e3854e28112bc8f329716bd9
Cunha Esther (CaP/ETS2) <cue2ca@bosch.com>
2024-04-08 16:36:33 -0300

    ...

--- src/components/Histórico/Link.tsx
+++ src/components/Histórico/Link.tsx
@@ -3,7 +3,7 @@ export default function Link() {
         <div className="bg-teamcolor w-[12em] h-[21px] rounded">
             <div className=" w-[20px] h-[20px] ml-2 flex items-center space-x-2">
                 <img src="../../src/assets/historico/link.png" className="w-[20px] h-[20px]" />
-                <a className="text-sm text-palette-blue">http://localhost:5173/</a>
+                <a href="http://localhost:5173/" className="text-sm text-palette-blue">link</a>
             </div>
 
         </div>
